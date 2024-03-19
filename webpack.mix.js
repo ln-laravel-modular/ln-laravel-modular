73feffe26262f6c950704b468e643aece5650077
langnang <langnang.chen@outlook.com>
2024-03-19 12:40:16 +0800

    mix

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,5 +12,24 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/app/js')
-    .sass('resources/sass/app.scss', 'public/app/css')
+    // .sass('resources/sass/app.scss', 'public/app/css')
+    .css('resources/css/app.css', 'public/app/css')
     .sourceMaps();
+
+// "@fortawesome/fontawesome-free": "^6.5.1",
+mix.copyDirectory('node_modules/@fortawesome/fontawesome-free/css', 'public/vendor/@fortawesome/fontawesome-free/6.5.1/css');
+mix.copyDirectory('node_modules/@fortawesome/fontawesome-free/js', 'public/vendor/@fortawesome/fontawesome-free/6.5.1/js');
+mix.copyDirectory('node_modules/@fortawesome/fontawesome-free/webfonts', 'public/vendor/@fortawesome/fontawesome-free/6.5.1/webfonts');
+// "axios": "^0.21.4",
+mix.copyDirectory('node_modules/axios/dist', 'public/vendor/axios/0.21.4');
+// "bootstrap": "^4.6.2",
+mix.copyDirectory('node_modules/bootstrap/dist', 'public/vendor/bootstrap/4.6.2');
+// "bootstrap-icons": "^1.11.3",
+mix.copyDirectory('node_modules/bootstrap-icons/font', 'public/vendor/bootstrap-icons/1.11.3');
+// "jquery": "^3.7.1",
+mix.copyDirectory('node_modules/jquery/dist', 'public/vendor/jquery/3.7.1');
+// "lodash": "^4.17.21",
+mix.copy('node_modules/lodash/lodash.js', 'public/vendor/lodash/4.17.21/lodash.js');
+mix.copy('node_modules/lodash/lodash.min.js', 'public/vendor/lodash/4.17.21/lodash.min.js');
+// "popper.js": "^1.16.1",
+mix.copyDirectory('node_modules/popper.js/dist/umd', 'public/vendor/popper.js/1.16.1');
